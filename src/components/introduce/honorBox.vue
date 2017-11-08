9ec8df8ebb0b577389a59c12e971e2b4d7132946
songStar <1043328710@qq.com>
2017-11-08 23:45:57 +0800

    update honor

--- src/components/introduce/honorBox.vue
+++ src/components/introduce/honorBox.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="honor">
-  <title-panel :title="title"></title-panel>
+  <title2-panel :title="title" :subtitle="subtitle"></title2-panel>
+  <img src="http://www.mapgoo.net/static/img/about/about-part3-img1.jpg" alt="" class="honor">
   	<el-row :gutter="10" class="honor-box">
   <el-col :xs="24" :sm="12" class="honor-item" v-for="item in honor" :key="item.title">
     <i class="fa-tag fa text-success"></i><span>{{item.title}}</span>
@@ -9,12 +10,12 @@
   </div>
 </template>
 <script>
-import {titlePanel} from '@/components/common'
+import {title2Panel} from '@/components/common'
 export default{
   data () {
     return {
-      title: '公司荣誉',
-      img: 'http://119.29.74.66/tm/Public/MallPic/honor/honor2.jpg',
+      title: 'OUR HONORS',
+      subtitle: '公司荣誉',
       honor: [{
         title: '国家高新技术企业、深圳市高新技术企业'
       }, {
@@ -33,7 +34,7 @@ export default{
     }
   },
   components: {
-    titlePanel
+    title2Panel
   }
 }
 </script>
@@ -43,13 +44,12 @@ export default{
     	margin-bottom: 50px;
     }
 	.honor .honor-item{
-		display: inline-block;
-	  float: left;
+    margin: 4px 0;
     text-align: left;
     font-size: 14px; 
     color: rgba(72, 106, 92, 0.8);
 	}
   .honor .honor-item i{
-    margin-right: 15px;
+    margin: 0 15px;
   }
 </style>
